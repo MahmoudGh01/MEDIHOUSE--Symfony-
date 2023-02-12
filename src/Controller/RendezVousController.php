43ce94a3e2bce6bfebcf3bca409910c221065055
MahmoudGh01 <80069718+MahmoudGh01@users.noreply.github.com>
2023-02-13 00:48:16 +0100

    CRUD

--- src/Controller/RendezVousController.php
+++ src/Controller/RendezVousController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\RendezVous;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\RendezVousRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RendezVousType;
  class RendezVousController extends AbstractController
 {
     #[Route('/rendez/vous', name: 'app_rendez_vous')]
@@ -15,4 +18,66 @@ use Symfony\Component\Routing\Annotation\Route;
             'controller_name' => 'RendezVousController',
         ]);
     }
-}
+
+    #[Route('/afficheRDV', name: 'afficheR')]
+
+      public function AfficheR(RendezVousRepository $repository)
+      {
+          $c= $repository->findAll();
+          return $this->render("rendez_vous/listR.html.twig", ["RendezVous"=>$c]);
+        }
+    
+        #[Route('/addRendezVous', name: 'app_addRendezVous')]
+        public function addRendezVous(ManagerRegistry $doctrine,Request $request)
+        {
+            $RendezVous= new RendezVous();
+            $form=$this->createForm(RendezVousType::class,$RendezVous);
+            
+            $form->handleRequest($request);
+            if($form->isSubmitted()){
+                $em =$doctrine->getManager() ;
+                $em->persist($RendezVous);
+                $em->flush();
+                return $this->redirectToRoute("afficheR");
+            }
+            return $this->renderForm("Rendez_Vous/addRendezVous.html.twig",
+                array("formClass"=>$form));
+         }
+         #[Route('/suppE/{id}', name: 'supprimerR')]
+
+        public function suppC(ManagerRegistry $doctrine,$id,RendezVousRepository $repository)
+          {
+          //récupérer le classroom à supprimer
+              $RendezVous= $repository->find($id);
+          //récupérer l'entity manager
+              $em= $doctrine->getManager();
+              $em->remove($RendezVous);
+              $em->flush();
+              return $this->redirectToRoute("afficheR");
+          } 
+          #[Route('/updatC/{id}', name: 'updateR')]
+
+    public function updatC(ManagerRegistry $doctrine,$id,RendezVousRepository $repository,Request $request)
+      {
+      //récupérer le RendezVous à supprimer
+          $RendezVous= $repository->find($id);
+          $newRendezVous= new RendezVous();
+          $form=$this->createForm(RendezVousType::class,$newRendezVous);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em =$doctrine->getManager() ;
+            $RendezVous->setIdRdv($newRendezVous->getIdRdv());
+            $RendezVous->setDocteur($newRendezVous->getDocteur());
+            $RendezVous->setPatient($newRendezVous->getPatient());
+            $RendezVous->setAdresse($newRendezVous->getAdresse());
+            $RendezVous->setdate($newRendezVous->getdate());
+            $em->flush();
+            return $this->redirectToRoute("afficheR");
+        }
+        return $this->renderForm("Rendez_Vous/updateR.html.twig",
+        array("formClass"=>$form));
+      } 
+
+    }
+
+
---
+++ src/Entity/RendezVous.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RendezVousRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RendezVousRepository::class)]
+class RendezVous
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    
+    #[ORM\Column(length: 255)]
+    public ?string $Id_Rdv = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Docteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Patient = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdRdv(): ?string
+    {
+        return $this->Id_Rdv;
+    }
+
+    public function setIdRdv(string $Id_Rdv): self
+    {
+        $this->Id_Rdv = $Id_Rdv;
+
+        return $this;
+    }
+
+    public function getDocteur(): ?string
+    {
+        return $this->Docteur;
+    }
+
+    public function setDocteur(string $Docteur): self
+    {
+        $this->Docteur = $Docteur;
+
+        return $this;
+    }
+
+    public function getPatient(): ?string
+    {
+        return $this->Patient;
+    }
+
+    public function setPatient(string $Patient): self
+    {
+        $this->Patient = $Patient;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+}
